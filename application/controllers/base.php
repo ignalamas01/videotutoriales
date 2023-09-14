49c6b02a16e041e595d1cefbdaf667dbb390f09e
Ugartex2 <ugartextex@gmail.com>
2023-09-14 19:04:49 -0400

    cambios composer demas

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-require FCPATH.'vendor/autoload.php';
+// require FCPATH.'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
