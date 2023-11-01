ab648aa6fd69482ffcac0ebd7ae6a6081203eab6
ignalamas01 <lamas.jose.8720@gmail.com>
2023-11-01 11:51:52 -0400

    este es de foros

--- application/views/foros.php
+++ application/views/foros.php
@@ -79,7 +79,7 @@
                             <!-- general form elements -->
                             <div class="card card-primary">
                                 <div class="card-header">
-                                    <h3 class="card-title">foros</h3>
+                                    <h3 class="card-title">FOROSSS</h3>
                                 </div>
 
                             
