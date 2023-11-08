a5782bc95af4e4042ab419aa723496e7a0030054
ignalamas01 <lamas.jose.8720@gmail.com>
2023-11-08 18:40:07 -0400

    terminado reporte graduados

--- application/views/graduados_lista.php
+++ application/views/graduados_lista.php
@@ -5,12 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>estudiantes graduados</h1>
+                    <h1>Estudiantes graduados</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <!-- <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>index.php/base/index">Home</a></li> -->
-                        <li class="breadcrumb-item active">graduados</li>
+                        <li class="breadcrumb-item active">Graduados</li>
                         <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>index.php/usuarios/logout">Cerrar sesión</a></li>
                     </ol>
                 </div>
