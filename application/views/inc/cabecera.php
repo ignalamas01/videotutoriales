86c3cf51d49afcd59d2d3af0ed79e186e822c512
ugartextex <ugartextex@gmail.com>
2024-10-13 15:21:26 -0400

    actualizacion en labtop

--- application/views/inc/cabecera.php
+++ application/views/inc/cabecera.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador</title>
+    <title>Administrador123</title>
 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>boostrap/css/bootstrap.css">
     <script src="<?php echo base_url(); ?>boostrap/js/bootstrap.js"></script>
